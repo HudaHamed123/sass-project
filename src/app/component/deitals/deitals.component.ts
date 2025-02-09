9b0c0ee00fa4e2b779ed45c4215710e4c0521fcb
HudaHamed123 <hodahamed546@gmail.com>
2025-02-09 22:29:38 +0200

    firstReady

---
+++ src/app/component/deitals/deitals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deitals',
+  imports: [],
+  templateUrl: './deitals.component.html',
+  styleUrl: './deitals.component.scss'
+})
+export class DeitalsComponent {
+
+}
